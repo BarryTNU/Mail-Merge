f59528afab0413ac4543398e7f2bf462cbd58ac8
BarryTNU <113223905+BarryTNU@users.noreply.github.com>
2024-01-08 10:43:51 +1000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox = New RichTextBox()
+        MenuStrip1 = New MenuStrip()
+        FileToolStripMenuItem = New ToolStripMenuItem()
+        NewToolStripMenuItem = New ToolStripMenuItem()
+        LetterToolStripMenuItem = New ToolStripMenuItem()
+        MailMergeToolStripMenuItem = New ToolStripMenuItem()
+        LoadToolStripMenuItem = New ToolStripMenuItem()
+        SaveAsToolStripMenuItem = New ToolStripMenuItem()
+        SaveToolStripMenuItem = New ToolStripMenuItem()
+        PrintToolStripMenuItem = New ToolStripMenuItem()
+        ExitToolStripMenuItem = New ToolStripMenuItem()
+        LoadToolStripMenuItem1 = New ToolStripMenuItem()
+        SaveToolStripMenuItem1 = New ToolStripMenuItem()
+        MergeToolStripMenuItem1 = New ToolStripMenuItem()
+        PrintToolStripMenuItem1 = New ToolStripMenuItem()
+        Lst_MailMerge = New ListBox()
+        ToolStripContainer1 = New ToolStripContainer()
+        Btn_Test = New Button()
+        Btn_Merge = New Button()
+        OpenFileDialog1 = New OpenFileDialog()
+        PrintPreviewControl1 = New PrintPreviewControl()
+        PrintDocument1 = New Printing.PrintDocument()
+        PageSetupDialog1 = New PageSetupDialog()
+        PrintDialog1 = New PrintDialog()
+        SaveFileDialog1 = New SaveFileDialog()
+        MenuStrip1.SuspendLayout()
+        ToolStripContainer1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' TextBox
+        ' 
+        TextBox.BorderStyle = BorderStyle.FixedSingle
+        TextBox.Location = New Point(217, 89)
+        TextBox.Margin = New Padding(4)
+        TextBox.Name = "TextBox"
+        TextBox.Size = New Size(1122, 678)
+        TextBox.TabIndex = 0
+        TextBox.Text = ""
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FileToolStripMenuItem, LoadToolStripMenuItem1, SaveToolStripMenuItem1, MergeToolStripMenuItem1, PrintToolStripMenuItem1})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(1406, 24)
+        MenuStrip1.TabIndex = 3
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FileToolStripMenuItem
+        ' 
+        FileToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {NewToolStripMenuItem, LoadToolStripMenuItem, SaveAsToolStripMenuItem, SaveToolStripMenuItem, PrintToolStripMenuItem, ExitToolStripMenuItem})
+        FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        FileToolStripMenuItem.Size = New Size(37, 20)
+        FileToolStripMenuItem.Text = "File"
+        ' 
+        ' NewToolStripMenuItem
+        ' 
+        NewToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {LetterToolStripMenuItem, MailMergeToolStripMenuItem})
+        NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        NewToolStripMenuItem.Size = New Size(112, 22)
+        NewToolStripMenuItem.Text = "New"
+        ' 
+        ' LetterToolStripMenuItem
+        ' 
+        LetterToolStripMenuItem.Name = "LetterToolStripMenuItem"
+        LetterToolStripMenuItem.Size = New Size(134, 22)
+        LetterToolStripMenuItem.Text = "Letter"
+        ' 
+        ' MailMergeToolStripMenuItem
+        ' 
+        MailMergeToolStripMenuItem.Name = "MailMergeToolStripMenuItem"
+        MailMergeToolStripMenuItem.Size = New Size(134, 22)
+        MailMergeToolStripMenuItem.Text = "Mail Merge"
+        ' 
+        ' LoadToolStripMenuItem
+        ' 
+        LoadToolStripMenuItem.Name = "LoadToolStripMenuItem"
+        LoadToolStripMenuItem.Size = New Size(112, 22)
+        LoadToolStripMenuItem.Text = "Load"
+        ' 
+        ' SaveAsToolStripMenuItem
+        ' 
+        SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        SaveAsToolStripMenuItem.Size = New Size(112, 22)
+        SaveAsToolStripMenuItem.Text = "Save as"
+        ' 
+        ' SaveToolStripMenuItem
+        ' 
+        SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        SaveToolStripMenuItem.Size = New Size(112, 22)
+        SaveToolStripMenuItem.Text = "Save"
+        ' 
+        ' PrintToolStripMenuItem
+        ' 
+        PrintToolStripMenuItem.Name = "PrintToolStripMenuItem"
+        PrintToolStripMenuItem.Size = New Size(112, 22)
+        PrintToolStripMenuItem.Text = "Print"
+        ' 
+        ' ExitToolStripMenuItem
+        ' 
+        ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        ExitToolStripMenuItem.Size = New Size(112, 22)
+        ExitToolStripMenuItem.Text = "Exit"
+        ' 
+        ' LoadToolStripMenuItem1
+        ' 
+        LoadToolStripMenuItem1.Name = "LoadToolStripMenuItem1"
+        LoadToolStripMenuItem1.Size = New Size(45, 20)
+        LoadToolStripMenuItem1.Text = "Load"
+        ' 
+        ' SaveToolStripMenuItem1
+        ' 
+        SaveToolStripMenuItem1.Name = "SaveToolStripMenuItem1"
+        SaveToolStripMenuItem1.Size = New Size(43, 20)
+        SaveToolStripMenuItem1.Text = "Save"
+        ' 
+        ' MergeToolStripMenuItem1
+        ' 
+        MergeToolStripMenuItem1.Name = "MergeToolStripMenuItem1"
+        MergeToolStripMenuItem1.Size = New Size(53, 20)
+        MergeToolStripMenuItem1.Text = "Merge"
+        ' 
+        ' PrintToolStripMenuItem1
+        ' 
+        PrintToolStripMenuItem1.Name = "PrintToolStripMenuItem1"
+        PrintToolStripMenuItem1.Size = New Size(44, 20)
+        PrintToolStripMenuItem1.Text = "Print"
+        ' 
+        ' Lst_MailMerge
+        ' 
+        Lst_MailMerge.BorderStyle = BorderStyle.FixedSingle
+        Lst_MailMerge.FormattingEnabled = True
+        Lst_MailMerge.ItemHeight = 21
+        Lst_MailMerge.Location = New Point(25, 89)
+        Lst_MailMerge.Name = "Lst_MailMerge"
+        Lst_MailMerge.Size = New Size(163, 674)
+        Lst_MailMerge.TabIndex = 4
+        ' 
+        ' ToolStripContainer1
+        ' 
+        ' 
+        ' ToolStripContainer1.ContentPanel
+        ' 
+        ToolStripContainer1.ContentPanel.Size = New Size(8, 0)
+        ToolStripContainer1.Location = New Point(426, 25)
+        ToolStripContainer1.Name = "ToolStripContainer1"
+        ToolStripContainer1.Size = New Size(8, 8)
+        ToolStripContainer1.TabIndex = 5
+        ToolStripContainer1.Text = "ToolStripContainer1"
+        ' 
+        ' Btn_Test
+        ' 
+        Btn_Test.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn_Test.Location = New Point(30, 47)
+        Btn_Test.Name = "Btn_Test"
+        Btn_Test.Size = New Size(49, 25)
+        Btn_Test.TabIndex = 6
+        Btn_Test.Text = "Test"
+        Btn_Test.UseVisualStyleBackColor = True
+        ' 
+        ' Btn_Merge
+        ' 
+        Btn_Merge.Font = New Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Btn_Merge.Location = New Point(121, 47)
+        Btn_Merge.Name = "Btn_Merge"
+        Btn_Merge.Size = New Size(67, 25)
+        Btn_Merge.TabIndex = 7
+        Btn_Merge.Text = "Merge"
+        Btn_Merge.UseVisualStyleBackColor = True
+        ' 
+        ' OpenFileDialog1
+        ' 
+        OpenFileDialog1.FileName = "OpenFileDialog1"
+        ' 
+        ' PrintPreviewControl1
+        ' 
+        PrintPreviewControl1.Location = New Point(410, 25)
+        PrintPreviewControl1.Name = "PrintPreviewControl1"
+        PrintPreviewControl1.Size = New Size(530, 32)
+        PrintPreviewControl1.TabIndex = 8
+        ' 
+        ' PrintDialog1
+        ' 
+        PrintDialog1.UseEXDialog = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 21F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1406, 812)
+        Controls.Add(PrintPreviewControl1)
+        Controls.Add(Btn_Merge)
+        Controls.Add(Btn_Test)
+        Controls.Add(ToolStripContainer1)
+        Controls.Add(Lst_MailMerge)
+        Controls.Add(TextBox)
+        Controls.Add(MenuStrip1)
+        Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        MainMenuStrip = MenuStrip1
+        Margin = New Padding(4)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ToolStripContainer1.ResumeLayout(False)
+        ToolStripContainer1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox As RichTextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LoadToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PrintToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LoadToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents MergeToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents PrintToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents Lst_MailMerge As ListBox
+    Friend WithEvents ToolStripContainer1 As ToolStripContainer
+    Friend WithEvents Btn_Test As Button
+    Friend WithEvents Btn_Merge As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents PrintPreviewControl1 As PrintPreviewControl
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents PageSetupDialog1 As PageSetupDialog
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents NewToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LetterToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MailMergeToolStripMenuItem As ToolStripMenuItem
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,359 @@
+﻿
+Imports System.Diagnostics.Metrics
+Imports System.IO
+Imports System.Net
+Imports System.Reflection
+Imports System.Runtime.Intrinsics
+Imports System.Windows.Forms.AxHost
+Imports Microsoft.VisualStudio.Shell.Interop
+
+
+Public Class Form1
+
+    'Public DefaultShortPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "MailMerge")
+    '  Public DefaultShortConfigPath = Path.Combine(DefaultShortPath, "Config")
+    Public DefaultLongPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) & Path.DirectorySeparatorChar & "MailMerge"
+
+
+    Dim fReader As StreamReader
+    Dim fWriter As StreamWriter
+    Public Letter_File_Path As String = ""
+    Dim Record_Field() As String
+
+    Public LetterMrg As String = ""
+    Public MergedLetter As String = ""
+    Public Letter_Text As String = ""
+    Public MemName As String = "Barry Campbell"
+    Public eMail As String = "barryTNU@gmail.com"
+    Public YtSailNr As String = "T1527"
+    Public TodaysDate As Date = Today
+    Public SeriesWinner As String = "Symphony"
+    Public WinnersPoints As String = "152"
+    Public SeriesPoints As String = "57"
+    Public SeriesPlace As String = "1"
+    Public YtOwner As String = "Barry Campbell"
+    Public XtianName As String = ""
+    Public CurrentRegatta As String = "EASTER2023"
+    Public YtName As String = "Jen"
+    Public YtClass As String = "Noelex"
+    Public ClassWinner As String = "The Gallon"
+    Public ClassPosition As String = "3"
+    Public NrInClass As String = "14"
+    Public ClassPoints As String = "87"
+    Public Type As String = "Trailer Yacht"
+    Public TypeWinner As String = "Mr Judge"
+    Public YtType As String = "Trailer Yacht"
+    Public NumberOfTypes As String = "4"
+    Public TypePosition As String = "6"
+    Public TypePoints As String = "58"
+    Public YachtsInRegatta As Integer = 32
+
+
+
+    Public File_Path As String = DefaultLongPath + Path.DirectorySeparatorChar ' This is the default file path  accessed by all modules
+    Public DocumentFilePath = File_Path & "Documents\"
+    Public LetterFilePath = File_Path & "Documents\Letters\"
+    Public MergeFilePath = File_Path & "Documents\Merge\"
+
+
+    ReadOnly ColourBlack = System.Drawing.Color.Black
+    ReadOnly ColourRed = System.Drawing.Color.Red
+    ReadOnly ColourGreen = System.Drawing.Color.LawnGreen
+    ReadOnly ColourAmber = System.Drawing.Color.Orange
+    ReadOnly ColourWhite = System.Drawing.Color.White
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load 'Handles MyBase.Load
+        'set up the form
+        Me.Text = "Mail Merge"
+        LoadListBox()
+        set_RTB_Indent(10)
+
+        'Create all these folders if they don't exist
+        CheckFolderExists(File_Path)
+        CheckFolderExists(DocumentFilePath)
+        CheckFolderExists(LetterFilePath)
+        CheckFolderExists(MergeFilePath)
+
+        Btn_Test.BackColor = ColourAmber
+        Btn_Merge.BackColor = ColourGreen
+        FileToolStripMenuItem.BackColor = ColourGreen
+        LoadToolStripMenuItem1.BackColor = ColourGreen
+        MergeToolStripMenuItem1.BackColor = ColourGreen
+        SaveToolStripMenuItem1.BackColor = ColourGreen
+        Btn_Merge.Visible = False
+        ' MergeToolStripMenuItem.Visible = False
+        MergeToolStripMenuItem1.Visible = False
+        PrintToolStripMenuItem.Visible = False
+        PrintToolStripMenuItem1.Visible = False
+        SaveToolStripMenuItem.Visible = False
+        SaveToolStripMenuItem1.Visible = False
+        SaveAsToolStripMenuItem.Visible = False
+
+
+    End Sub
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    Private Sub MergeToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles MergeToolStripMenuItem1.Click, Btn_Merge.Click
+        Dim fPath As String = MergeFilePath
+        CheckFolderExists(fPath)
+        Dim StringToMerge As String = TextBox.Text
+        PrintToolStripMenuItem.Visible = True
+        PrintToolStripMenuItem1.Visible = True
+        LetterMrg = ParseDoc(StringToMerge)
+        '' This is the Play Room
+        'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+
+
+
+
+
+
+
+
+
+
+
+        'MessageBox.Show(NewString)
+
+        '  TextBox.Text = LetterMrg
+        fWriter = New StreamWriter(fPath & "MergedLetter.txt")
+        fWriter.Write(LetterMrg)
+        fWriter.Close()
+
+        'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    End Sub
+
+
+    ' ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ
+    'All subs below are to do with setting  up the form, Loading and saving, and other housekeeping
+    Sub LoadListBox()
+        With Lst_MailMerge
+
+            .Items.Add("{TodaysDate}")
+            .Items.Add("{MemName}")
+            .Items.Add("{YtSailNr}")
+            .Items.Add("{YtName}")
+            .Items.Add("{YtOwner}")
+            .Items.Add("{XtianName}")
+            .Items.Add("{eMail}")
+
+            .Items.Add("{Address1}")
+            .Items.Add("{Address2}")
+            .Items.Add("{Town}")
+            .Items.Add("{PostCode}")
+            .Items.Add("{State}")
+            .Items.Add("{Country}")
+
+
+            .Items.Add("{YachtsInRegatta}")
+            .Items.Add("{WinnersPoints")
+            .Items.Add("{SeriesWinner}")
+            .Items.Add("{SeriesPoints}")
+            .Items.Add("{SeriesPlace}")
+            .Items.Add("{CurrentRegatta}")
+            .Items.Add("{YtClass}")
+            .Items.Add("{YtSkipper}")
+            .Items.Add("{ClassWinner}")
+            .Items.Add("{ClassPosition}")
+            .Items.Add("{NrInClass}")
+            .Items.Add("{ClassPoints}")
+            .Items.Add("{TypeWinner}")
+            .Items.Add("{YtType}")
+            .Items.Add("{NumberOfTypes}")
+            .Items.Add("{TypePosition}")
+            .Items.Add("{TypePoints}")
+
+        End With
+
+        My.Computer.Clipboard.Clear()
+
+    End Sub
+
+
+    Sub Testfile(sender As Object, e As EventArgs)
+
+
+        Dim l As Integer = InStr(MemName, " ")
+        Me.XtianName = Mid(MemName, 1, l)
+
+
+        Dim lett2 = "Dear " & XtianName & "
+
+Thank you for participating in our " & CurrentRegatta & " regatta.
+
+Here are the results of this regatta, and your individual results.
+
+The overall winner of the Regatta was " & SeriesWinner & " with " & WinnersPoints & " Points.
+
+Your yacht " & YtName & " was placed " & SeriesPlace & " with " & SeriesPoints & " points.
+
+Your yacht was also entered in the " & YtClass & " Class, and was competing with " & NrInClass & " other yachts in that class, and also  " & NumberOfTypes & " similar yachts in the " & YtType & " category.
+
+The winning " & YtClass & " was " & ClassWinner & ", and your position in this class was " & ClassPosition & ", with " & ClassPoints & " points.
+
+The winning " & YtType & " was " & TypeWinner & ", and your position was " & TypePosition & " with " & TypePoints & " Points.
+
+Thank you again for supporting our club. We look forward to seeing you at our next regatta. 
+
+Yours sincerely
+
+
+Commodore"
+
+        MsgBox(lett2)
+
+    End Sub
+
+    Private Sub Lst_MailMerge_Click(sender As Object, e As EventArgs) Handles Lst_MailMerge.Click 'Handles Lst_MailMerge.Click
+        My.Computer.Clipboard.SetText(Lst_MailMerge.SelectedItem, TextDataFormat.Text) 'Copy to Clipboard
+    End Sub
+
+    Private Sub TextBox_Click(sender As Object, e As EventArgs) Handles TextBox.Click 'Handles TextBox.Click
+        TextBox.Paste() 'paste from clipboard
+        My.Computer.Clipboard.Clear()
+
+    End Sub
+
+    Private Sub Btn_Test_Click(sender As Object, e As EventArgs) Handles Btn_Test.Click 'Handles Btn_Test.Click
+        Testfile(sender, e)
+    End Sub
+
+    Private Sub LoadToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoadToolStripMenuItem.Click, LoadToolStripMenuItem1.Click
+        Dim fPath = LetterFilePath
+        Dim MergeFile As String
+
+        OpenFileDialog1.Title = "Please select a  file"
+        OpenFileDialog1.InitialDirectory = fPath
+        OpenFileDialog1.Filter = "Text Files(*.txt)|*.txt|Merge Files(*.mrg)|*.mrg|All Files(*.*)|*.*"
+        OpenFileDialog1.FileName = ""
+
+        Try
+            If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+                Letter_File_Path = OpenFileDialog1.FileName
+                fReader = New StreamReader(Letter_File_Path)
+                Do While fReader.Peek <> -1
+                    MergeFile = fReader.ReadToEnd
+                Loop
+                fReader.Close()
+                TextBox.Text = MergeFile
+
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message, vbCritical, "File Read Error")
+        End Try
+
+    End Sub
+
+
+    Private Sub SaveAsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveAsToolStripMenuItem.Click
+
+        Dim fPath = File_Path & "Documents\Letters"
+        Dim RTB_Text As String = TextBox.Text
+
+        CheckFolderExists(fPath & "\")
+
+        SaveFileDialog1.Title = "Please choose a file path and name"
+        SaveFileDialog1.InitialDirectory = fPath
+        SaveFileDialog1.Filter = "Text Files(*.txt)|*.txt|Merge Files(*.mrg)|*.mrg|All Files(*.*)|*.*"
+        SaveFileDialog1.FileName = Letter_File_Path
+        Try
+            If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+                Letter_File_Path = SaveFileDialog1.FileName
+                fWriter = New StreamWriter(Letter_File_Path)
+                fWriter.Write(RTB_Text)
+                fWriter.Close()
+
+            End If
+
+        Catch EX As Exception
+            MsgBox(EX.Message, vbCritical, "Error writing file")
+        End Try
+
+    End Sub
+
+
+
+    Private Sub PrintToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles PrintToolStripMenuItem1.Click
+
+    End Sub
+
+    Private Sub SaveToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click, SaveToolStripMenuItem1.Click
+        If Letter_File_Path = "" Then SaveAsToolStripMenuItem_Click(sender, e)
+
+        Dim fPath As String = ""
+        Dim RTB_Text As String = TextBox.Text
+
+        Try
+            fWriter = New StreamWriter(Letter_File_Path)
+            fWriter.Write(RTB_Text)
+            fWriter.Close()
+        Catch EX As Exception
+            MsgBox(EX.Message, vbCritical, "Error writing file")
+        End Try
+    End Sub
+
+    Function ParseDoc(StrLetter As String)
+        Dim LetterText As String = ""
+        StrLetter = ChrW(34) & StrLetter : StrLetter += ChrW(34) ' Add quotes to the beginning and end of the letter
+        LetterText = Replace(StrLetter, ChrW(123), ChrW(32) & ChrW(34) & " & " & ChrW(32), 1) 'Replace {
+        StrLetter = Replace(LetterText, ChrW(125), ChrW(32) & " & " & ChrW(34) & ChrW(32), 1) ' Replace }
+        ParseDoc = StrLetter
+    End Function
+
+    Sub CheckFolderExists(FolderName)
+        Dim letter As String = ""
+
+        For i = Len(FolderName) To 0 Step -1
+            letter = Mid(FolderName, i, 1)
+            If letter = "\" Then
+                FolderName = Mid(FolderName, 1, i - 1)
+                Exit For
+            End If
+        Next
+        Dim FolderExists As Boolean = IO.Directory.Exists(FolderName)
+        If FolderExists = False Then
+            MkDir(FolderName)
+
+        End If
+    End Sub
+    Sub set_RTB_Indent(indent As Integer)
+        'sets the Rich Text Box indent to 10
+        With TextBox
+            Dim OldSelStart As Integer = .SelectionStart
+            Dim OldSelLen As Integer = .SelectionLength
+            .SelectionStart = 0
+            .SelectionLength = Len(.Text)
+            .SelectionIndent = indent
+            .SelectionStart = OldSelStart
+            .SelectionLength = OldSelLen
+        End With
+    End Sub
+
+    Private Sub TextBox_TextChanged(sender As Object, e As EventArgs) Handles TextBox.TextChanged 'Handles TextBox.TextChanged
+        ' MergeToolStripMenuItem.Visible = True
+        MergeToolStripMenuItem1.Visible = True
+        SaveToolStripMenuItem.Visible = True
+        SaveToolStripMenuItem1.Visible = True
+        SaveAsToolStripMenuItem.Visible = True
+        Btn_Merge.Visible = True
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click 'Handles ExitToolStripMenuItem.Click
+        Me.Dispose()
+    End Sub
+
+    Private Sub NewToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub LetterToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LetterToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub MailMergeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MailMergeToolStripMenuItem.Click
+
+    End Sub
+End Class
